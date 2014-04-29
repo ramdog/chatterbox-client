1e986c35300493373a2e99ade1c44a40d296929a
Hack Reactor Students <talegram@gmail.com>
2014-04-28 20:28:28 -0700

    sending msg, receving msg, rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,95 @@
-// YOUR CODE HERE:
+var app = {
+  currentRoom: undefined, //initialize with no room
+  init: function() {
+    var queryString = window.location.search;
+    var username = queryString.substring(queryString.indexOf("=") + 1);
+    this.refresh();
+    $('#input').on('submit', function(event) {
+      event.preventDefault();
+      var $message = $('#chat_message');
 
+      app.send({
+        text: $message.val(),
+        username: username,
+        roomname: app.currentRoom === undefined || app.currentRoom === "all" ? "" : app.currentRoom
+      });
+      $message.val("");
+      app.fetch();
+    });
+
+    var $roomSelect = $('#roomselect');
+    $roomSelect.on('change' , function(event) {
+      event.preventDefault();
+      app.currentRoom = $roomSelect.val();
+    });
+
+  },
+  send: function(message) {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function(roomname) {
+    var data = {order: '-createdAt'};
+    if (roomname !== undefined && roomname !== 'all') {
+      data.where = {roomname: roomname};
+    }
+
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      contentType: 'application/json',
+      data: data,
+      success: function (data) {
+
+        $("#chats").empty();
+        var results = data.results.slice(0,5).reverse();
+
+        // <div class='updated_at'>" + _.escape(message.updatedAt) + "</div>\
+        _.each(results, function(message) {
+          $("#chats").append(
+            "<div class='chat'>\
+              <div class='username'>" + _.escape(message.username) + "</div>\
+              <div class='updated_at'>" + _.escape(message.updatedAt) + "</div>\
+              <div class='roomname'>" + _.escape(message.roomname) + "</div>\
+              <div class='message'>" + _.escape(message.text) + "</div>\
+            </div>");
+        });
+      },
+
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch messages');
+      }
+    });
+  },
+  refreshRooms: function() {
+
+  },
+  _refreshInternal: function() {
+    app.fetch(app.currentRoom);
+    app.refreshRooms();
+  },
+  refresh: function() {
+    app.fetch();
+    setInterval(app._refreshInternal, 1000);
+  }
+};
+
+/**
+ * createdAt: "2013-10-07T16:22:03.280Z"
+objectId: "teDOY3Rnpe"
+roomname: "lobby"
+text: "hello"
+updatedAt: "2013-10-07T16:22:03.280Z"
+username: "gary"
+ */
